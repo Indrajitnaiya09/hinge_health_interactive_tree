04d8c874aa639aade964c22235b4de7d3e15e3d0
Indrajit Naiya <indrajitnaiya09@gmail.com>
2023-09-15 14:09:43 +0530

    imp: inserting node to the appropriate position -- create action added -- random number generator added -- params.permit implemented -- code refactored

--- app/models/tree_node.rb
+++ app/models/tree_node.rb
@@ -17,4 +17,70 @@ class TreeNode
   def self.tree
     @@TREE
   end
+
+  def self.link_to_tree(interactive_node, root_index = nil)
+    # parent id passed but root_index not passed
+    # then consider root_index = 0, and perform the rest operation
+    #
+    # if parent id not found, return error
+    parent_id = interactive_node.parent_id
+    if parent_id
+      return interactive_node.process_insert_request(parent_id, root_index) if root_index
+
+      root_index = 0
+      interactive_node.process_insert_request(parent_id, root_index)
+    else
+      # if parent id not present, consider as root level entry
+      insert_in_tree_array(interactive_node)
+    end
+  end
+
+  def self.insert_in_tree_array(node)
+    @@TREE << node.format_for_output
+  end
+
+  def insert_record(children_arr, node)
+    children_arr.push(node.format_for_output)
+    children_arr
+  end
+
+  def process_insert_request(parent_id, root_index)
+
+    # find_parent
+    current_tree = @@TREE[root_index]
+    parent = if current_tree.key?(parent_id)
+               current_tree[parent_id]
+             else
+               find_parent_node(children_nodes(current_tree),
+                                parent_id)
+             end
+
+    # insert new record inside parent
+    link_node_to_parent(parent, self)
+  end
+
+  def link_node_to_parent(parent, node)
+    insert_record(parent[:children], node)
+    node
+  end
+
+  def children_nodes(current_tree)
+    current_tree.values[0][:children]
+  end
+
+  def find_parent_node(data, search_id)
+    data.each do |item|
+      # Check if the current item has an :id key and its value matches the search_id
+      return item[search_id] if item.key?(search_id)
+
+      # Recursively search in the children if they exist
+      result = find_parent_node(children_nodes(item), search_id)
+      return result if result
+    end
+    nil # ID not found
+  end
+
+  def format_for_output
+    { id.to_s => { label: label, children: children } }
+  end
 end
